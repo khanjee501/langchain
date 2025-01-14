73fc55550fea14a16c4d41c2700814f0f695d915
Saro <57326765+khanjee501@users.noreply.github.com>
2025-01-14 22:48:40 +0500

    adding better descriptions to tool arguments using args_schema, pydantic and typing

--- ecommerce_agents/tools/sql.py
+++ ecommerce_agents/tools/sql.py
@@ -1,4 +1,6 @@
 import sqlite3
+from pydantic.v1 import BaseModel
+from typing import List
 from langchain.tools import Tool
 
 
@@ -34,11 +36,22 @@ def describe_tables(table_names):
     return "\n".join(row[0] for row in rows if row[0] is not None)
 
 
+# this is used so that the function code has meaningful json objects instead of onjects like __arg1
+# now it will show query and of type string. So thi way gpt can understand the context better
+class RunQueryArgsSchema(BaseModel):
+    query: str
+
+
 run_query_tool = Tool.from_function(
     name="run_sqlite_query",
     description="Run a sqlite query.",
     func=run_sqlite_query,
+    args_schema=RunQueryArgsSchema,
 )
+
+
+class DescribeTablesArgsSchema(BaseModel):
+    table_names: List[str]
 
 
 # describe tables tools to return the schema of the tables of the db
@@ -46,4 +59,5 @@ describe_tables_tool = Tool.from_function(
     name="describe_tables",
     description="Given a list of table names, return the schema of those tables",
     func=describe_tables,
+    args_schema=DescribeTablesArgsSchema,
 )
