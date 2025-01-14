715dc7bae5d6f25b021f2577002f68fd44f45f45
Saro <57326765+khanjee501@users.noreply.github.com>
2025-01-14 20:52:33 +0500

    imported db file, implemented tools, agent and agent_executor

---
+++ ecommerce_agents/main.py
@@ -0,0 +1,40 @@
+from tabnanny import verbose
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+)
+from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from dotenv import load_dotenv
+
+from tools.sql import run_query_tool
+
+load_dotenv()
+
+chat = ChatOpenAI()
+
+prompt = ChatPromptTemplate(
+    messages=[
+        HumanMessagePromptTemplate.from_template("{input}"),
+        MessagesPlaceholder(
+            variable_name="agent_scratchpad",
+        ),
+    ]
+)
+
+tool = [run_query_tool]
+
+agent = OpenAIFunctionsAgent(
+    llm=chat,
+    prompt=prompt,
+    tools=tool,
+)
+
+agent_executor = AgentExecutor(
+    agent=agent,
+    verbose=True,
+    tools=tool,
+)
+
+agent_executor("How many users are there in the database?")
