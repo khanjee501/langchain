fb50d79c46de102b435e8fc62c6e488cc3faefd8
Saro <57326765+khanjee501@users.noreply.github.com>
2025-01-14 18:15:23 +0500

    implemented custom retriever to avoid duplicate documents responses and duplicate docs using RedundantFilterRetriever that inherits from BaseRetreiver

--- facts/prompt.py
+++ facts/prompt.py
@@ -2,6 +2,7 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores.chroma import Chroma
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
+from redundant_filter_retriever import RedundantFilterRetriever
 import langchain
 from dotenv import load_dotenv
 
@@ -18,7 +19,12 @@ db = Chroma(
     embedding_function=embeddings,
 )
 
-retriever = db.as_retriever()
+# retriever = db.as_retriever()
+
+retriever = RedundantFilterRetriever(
+    embeddings=embeddings,
+    chroma=db,
+)
 
 chain = RetrievalQA.from_chain_type(
     llm=chat,
